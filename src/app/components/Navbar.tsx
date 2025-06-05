79446cf96e1683e6d6b6a8a3f0e7ad97f62687ef
maauit03 <martinau626@gmail.com>
2025-06-06 00:37:24 +0200

    update toggle navbar and enable smooth scrolling

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,82 +1,106 @@
-import React from 'react'
+'use client'
+import { motion } from "motion/react";
+import { useEffect, useState } from "react";
+import Path from "./Path";
+import { useRef } from 'react';
 
-const Navbar = () => {
-    return (
-    <div>
-        <nav className="bg-yellow-300 rounded-4xl w-2xl ">
-        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
-            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* <!-- Mobile menu button--> */}
-                <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset" aria-controls="mobile-menu" aria-expanded="false">
-                <span className="absolute -inset-0.5"></span>
-                <span className="sr-only">Open main menu</span>
-                {/* <!--
-                    Icon when menu is closed.
+export default function Navbar() {
+  const [expanded, setExpanded] = useState(true);
 
-                    Menu open: "hidden", Menu closed: "block"
-                --> */}
-                <svg className="block size-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                </svg>
-                {/* <!--
-                    Icon when menu is open.
+function scrollToChapter(chapter: string) {
+  let elementId: string | null = null;
 
-                    Menu open: "block", Menu closed: "hidden"
-                --> */}
-                <svg className="hidden size-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-                </svg>
-                </button>
-            </div>
-            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex shrink-0 items-center">
-                </div>
-                <div className="hidden sm:ml-6 sm:block">
-                <div className="flex space-x-4">
-                    {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
-                    <a href="#" className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">About Me</a>
-                    <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-700 hover:text-white">Resume</a>
-                    <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-700 hover:text-white">Projects</a>
-                    <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-700 hover:text-white">Contact</a>
-                </div>
-                </div>
-            </div>
-            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button type="button" className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden">
-                <span className="absolute -inset-1.5"></span>
-                <span className="sr-only">View notifications</span>
-                <svg className="size-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
-                </svg>
-                </button>
-
-                {/* <!-- Profile dropdown --> */}
-                <div className="relative ml-3">
-                <div>
-                    <button type="button" className="relative flex rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                    <span className="absolute -inset-1.5"></span>
-                    <span className="sr-only">Open user menu</span>
-                    </button>
-                </div>
-                </div>
-            </div>
-            </div>
-        </div>
-
-        {/* <!-- Mobile menu, show/hide based on menu state. --> */}
-        <div className="sm:hidden" id="mobile-menu">
-            <div className="space-y-1 px-2 pt-2 pb-3">
-            {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
-            <a href="#" className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Dashboard</a>
-            <a href="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Team</a>
-            <a href="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a>
-            <a href="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Calendar</a>
-            </div>
-        </div>
-        </nav>
-    </div>
-  )
+  switch (chapter) {
+    case "aboutme":
+      elementId = "aboutme";
+      break;
+    case "education":
+      elementId = "education";
+      break;
+    case "work":
+      elementId = "work";
+      break;
+    case "projects":
+      elementId = "projects";
+      break;
+    case "skills":
+      elementId = "skills";
+      break;
+    case "contact":
+      elementId = "contact";
+      break;
+    default:
+      elementId = null;
+  }
+  if (elementId) {
+    document.getElementById(elementId)?.scrollIntoView({ behavior: "smooth" });
+  }
 }
 
-export default Navbar
+
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setExpanded(true); // Expand when at the top
+      } else {
+        //setExpanded(false); // Collapse when scrolling down
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+
+  return (
+    <div className="flex items-center justify-center overflow-hidden">
+        <motion.div
+        initial={false}
+        animate={expanded ? "expanded" : "collapsed"}
+        variants={{
+          collapsed: {
+            width: 100,
+            height: 100,
+            borderRadius: "30%",
+            backgroundColor: "black",
+            justifyContent: "center",
+            alignItems: "center",
+          },
+          expanded: {
+            width: "100vw",
+            height: 100,
+            borderRadius: "0%",
+            backgroundColor: "black",
+            justifyContent: "space-between",
+            alignItems: "center",
+          },
+        }}
+        transition={{ duration: .75, ease: "easeIn" }}
+        className="flex px-10"
+        onClick={() => setExpanded(!expanded)}
+      >
+        {expanded && (
+        <nav className="flex w-full justify-between items-center text-white font-medium text-lg">
+          <div className="flex gap-1">
+            <Path />
+          </div>
+          <ul className="flex gap-8 p-8" onClick={(e) => e.stopPropagation()}>
+            <li><button onClick={() => scrollToChapter("aboutme")}>About Me</button></li>
+            <li><button onClick={() => scrollToChapter("projects")}>Projects</button></li>
+            <li><button onClick={() => scrollToChapter("skills")}>Skills</button></li>
+            <li><button onClick={() => scrollToChapter("education")}>Education</button></li>
+            <li><button onClick={() => scrollToChapter("work")}>Work Experience</button></li>
+            <li><button onClick={() => scrollToChapter("contact")}>Contact</button></li>
+          </ul>
+        </nav>
+        )}
+        {!expanded && (
+            <div>
+                Click me!
+            </div>
+        )}
+      </motion.div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,51 @@
-
+'use client'
+import { useRef } from 'react';
 import AboutMe from './components/AboutMe';
 import Hero from './components/Hero';
-import Hologram from './components/Models/Hologram';
 import Navbar from './components/Navbar';
-import Path from './components/Path';
+import Projects from './components/Projects';
 import Skills from './components/Skills';
-import StartButton from './components/StartButton/StartButton';
 import Timeline from './components/Timeline';
+import Contact from './components/Contact';
 export default function Home() {
+    const skillsRef = useRef<HTMLDivElement | null>(null);
+
   return (
-    <main className="">
+    <main>
       {/* Top section with Path (left) and centered Navbar */}
-      <div className="relative flex items-center mt-8 mb-8 h-16">
-        {/* Left element */}
-        <div className="ml-8 z-10">
-          <Path />
-        </div>
-        {/* Centered Navbar */}
-        <div className="absolute left-1/2 transform -translate-x-1/2">
-          <Navbar />
-        </div>
+    <div className="relative flex items-center mt-4 mb-8 h-16 z-100">
+      <div className="fixed z-50">
+        <Navbar />
       </div>
-
-
+    </div>
       {/* Main content section */}
-      <section className="min-h-screen bg-zinc-800 text-white font-mono">
-        <AboutMe/>
-        <div className='mt-8'>
-        <Hero />
-        </div>
-        <Skills/>
-        <Timeline />
-      </section>
+<section className="min-h-screen bg-zinc-800 text-white font-mono">
+  <div id="aboutme" className="scroll-mt-24">
+    <AboutMe />
+  </div>
+
+  <div id="projects" className="scroll-mt-30">
+    <Projects />
+  </div>
+
+  <div id="skills" className="scroll-mt-24">
+    <Skills />
+  </div>
+
+  <div id="education" className="scroll-mt-24">
+    <Timeline type="education" />
+  </div>
+
+  <div id="work" className="scroll-mt-24">
+    <Timeline type="work" />
+  </div>
+
+  <div id="contact" className="scroll-mt-24">
+    <Contact />
+  </div>
+</section>
+
     </main>
+
   );
 }
